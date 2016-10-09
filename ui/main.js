fd93cdea11afc9affa88489b480598b1084d3b69
shobab <guggilashobabu@iiitdmj.ac.in>
2016-10-10 01:22:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var mleft = 0;
+function mright(){
+    var img = document.getElementById('madi');
+    mleft+=5;
+    img.style.marginLeft = mleft + 'px';
+}
+
+img.onclick() = function(){
+    var interval = setInterval(mright,50);
+};
