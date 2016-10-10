ad99364a096da7218eb1ebbb3caf2d2fdf39d70f
shobab <guggilashobabu@iiitdmj.ac.in>
2016-10-10 22:12:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,11 @@ function mright(){
 img.onclick = function(){
     var interval = setInterval(mright,50);
 };
+
+var counter = 0;
+var btn = document.getElementById('b1');
+btn.onclick = function(){
+    counter = counter+1;
+    var span = document.getElementById('num');
+    span.innerHTML = counter.toString();
+};
