f7fd49d9c6246bdb41c613e5ad30fafe2725e9a1
shobab <guggilashobabu@iiitdmj.ac.in>
2016-10-10 22:38:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,15 +14,15 @@ var counter1 = 0;
 var btn = document.getElementById('b1');
 btn.onclick = function(){
     var request = new XMLHttpRequest();
-    request.onreadychangestate = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
+    //request.onreadychangestate = function(){
+        //if(request.readyState === XMLHttpRequest.DONE){
+            //if(request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('num');
                 span.innerHTML = counter.toString();
-            }
-        }
-    };
+            //}
+        //}
+    //};
     request.open('GET','http://shobab.imad.hasura-app.io/counter',true);
     request.send(null);
 };
